3ee2bd79ee9fdaf469b51c166bae27b646e1d3fb
mrandersongb <mr.anderson.brito@gmail.com>
2022-01-09 22:11:42 -0300

    The singleton pattern class was created.

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -6,7 +6,19 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SingletonPattern singletonPattern1 = SingletonPattern.Instance();
+
+            SingletonPattern singletonPattern2 = SingletonPattern.Instance();
+
+            if(singletonPattern1.Equals(singletonPattern2))
+            {
+                Console.WriteLine("singletonPattern1 is equal singlePattern2");
+            }
+
+            if (singletonPattern1 == singletonPattern2)
+            {
+                Console.WriteLine("singletonPattern1 is equal singlePattern2");
+            }
         }
     }
 }
---
+++ SingletonPattern/SingletonPattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// The 'Singleton' class example
+    /// </summary>
+    sealed class SingletonPattern
+    {
+        static readonly SingletonPattern instance = new SingletonPattern();
+
+        public static SingletonPattern Instance()
+        {
+            return instance;
+        }
+
+        private SingletonPattern() { }
+    }
+}
